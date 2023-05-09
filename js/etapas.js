363a0ab6abe6f3bb891d7f80fe4f8a2913209e4f
Letícia Pereira Fernades <127545256+lele-16@users.noreply.github.com>
2023-05-08 21:23:08 -0300

    Add files via upload

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos:[
+        {
+            numero:'12345',
+            nome: 'Alpacino',
+            partido: 'Scarface',
+            fotos:[
+            {url:'alpachino.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        {
+            numero:'67891',
+            nome: 'Wendy Torrance',
+            partido: 'Iluminado',
+            fotos:[
+            {url:'wendy torrance.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        ]
+},
+{
+    titulo: 'PREFEITO',
+    numeros:2,
+    candidatos:[
+        {
+            numero: '99',
+            nome: 'Mia Goth',
+            partido: 'XXX',
+            vice: 'Natasha',
+            fotos:[
+                {url: 'x (2022) (1).jpg', legenda: 'Prefeito'},
+                {url: 'transferir (1).jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+        {
+            numero: '10',
+            nome: 'Anya Taylor',
+            partido: 'Taxi Drive',
+            vice: 'travis Brickle',
+            fotos:[
+                {url: 'anya taylor.jpg', legenda: 'Prefeito'},
+                {url: 'Travis Brickle.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+    ]
+}
+];
